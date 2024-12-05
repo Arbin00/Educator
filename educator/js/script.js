a5b1fc3fc50eea52df82b4e3f4f0ac3c65233c90
Arbin00 <105848098+Arbin00@users.noreply.github.com>
2024-12-06 00:17:45 +0545

    Add files via upload

---
+++ educator/js/script.js
@@ -0,0 +1,15 @@
+//change navbar tyles on scroll 
+
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 100)
+})
+
+//show/hide faq answer
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+    })
+})
